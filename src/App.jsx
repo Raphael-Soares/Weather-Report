3a00e70f49d4427301735853d38ba5649614439a
Raphael-Soares <soaresphaell@gmail.com>
2023-04-17 20:04:12 -0300

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import {useEffect, useState} from "react";
-import {GoLocation} from "react-icons/go";
-import {FaTemperatureLow} from "react-icons/fa";
-import {BiCurrentLocation} from "react-icons/bi";
-import {AiOutlineSearch} from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { GoLocation } from "react-icons/go";
+import { FaTemperatureLow } from "react-icons/fa";
+import { BiCurrentLocation } from "react-icons/bi";
+import { AiOutlineSearch } from "react-icons/ai";
+
+import axios from "axios";
 
 const API_KEY = "547dc05df3f5c6a8bbd209e895b43ad9";
 
@@ -25,30 +27,43 @@ function App() {
     }
 
     async function getDataLocation(lat, lon) {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=pt_br`);
-        const respJson = await response.json();
-        if (respJson.cod !== "200") {
-            setData(respJson);
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=pt_br`
+            );
+            setData(response.data);
             setSearch("");
+        } catch (error) {
+            console.log(error);
         }
     }
 
     async function getData() {
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${search.length == 0 ? " " : search}&appid=${API_KEY}&units=metric&lang=pt_br`
-        );
-        const respJson = await response.json();
-        setData(respJson);
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${
+                    search.length === 0 ? " " : search
+                }&appid=${API_KEY}&units=metric&lang=pt_br`
+            );
+            setData(response.data);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     useEffect(() => {
-        getLocation();
-    }, [search]);
+        getData();
+    }, []);
 
     return (
         <div className="App">
             <div className="controls">
-                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Cidade" />
+                <input
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Cidade"
+                />
                 <button onClick={() => getData(search)}>
                     <AiOutlineSearch />
                 </button>
@@ -64,7 +79,9 @@ function App() {
                             <GoLocation />
                             {data.name}
                         </div>
-                        <div className="weather-card__header__date">{new Date().toLocaleDateString()}</div>
+                        <div className="weather-card__header__date">
+                            {new Date().toLocaleDateString()}
+                        </div>
                     </div>
                     <div className="weather-card__body">
                         <div className="weather-card__body__temp">
@@ -72,7 +89,8 @@ function App() {
                             {data.main.temp}&deg;C
                         </div>
                         <div className="weather-card__body__description">
-                            {data.weather[0].description[0].toUpperCase() + data.weather[0].description.substring(1)}
+                            {data.weather[0].description[0].toUpperCase() +
+                                data.weather[0].description.substring(1)}
                         </div>
                     </div>
                 </div>
